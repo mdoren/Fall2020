d723bc9da8e58a8b0b5bdc7c2893946a042ed0e8
Matt Doren <mattdoren62@gmail.com>
2020-09-07 15:11:26 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,6 @@
+fun main(){
+    println("I'm very tired.")
+    println("I recently started collecting Jordans. It's an expensive hobby!")
+    println("Despite 2020 being a not-so-great year for most, it's been the best year of my life personally.")
+    println("I'm still struggling to hold myself accountable for online classes, but I know I can do it.")
+}
